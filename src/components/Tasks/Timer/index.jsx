db0bf8119b909cd729511ab9aede1b428acc9894
Olena kuzii <okuziy@gmail.com>
2021-04-09 17:11:47 +0300

    add timer

--- src/components/Tasks/Timer/index.jsx
+++ src/components/Tasks/Timer/index.jsx
@@ -1,12 +1,35 @@
 import './index.scss';
+import React, { useState, useEffect } from 'react';
+
 
 function Timer() {
+
+  const [count, setCount] = useState(0);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    let timer = null;
+    if (start) {
+      timer = setInterval(() => {
+        setCount(count => count + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [count, start]);
+
+  const reset = () => {
+    setCount(0);
+    setStart(false);
+  }
+
   return (
     <div className="Timer">
-      <button>Start</button>
-      <button>Stop and Reset</button>
+      <button onClick={() => setStart(true)}>Start</button>
+      <button onClick={reset}>Stop and Reset</button>
 
-      <div className="seconds">0</div>
+      <div className="seconds">{count}</div>
     </div>
   );
 }
