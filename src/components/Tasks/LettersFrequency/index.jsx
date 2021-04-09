2012057d9da60537bd1b6bc7c42a853a20f82f72
Olena kuzii <okuziy@gmail.com>
2021-04-09 16:13:17 +0300

    add calculateFrequency function

--- src/components/Tasks/LettersFrequency/index.jsx
+++ src/components/Tasks/LettersFrequency/index.jsx
@@ -5,8 +5,31 @@ import Textarea from './Textarea';
 import Chart from './Chart';
 
 export const calculateFrequency = (text = '') => {
-  // your implementation should be inside this function
-  // please don't change anything outside
+  let cleanText = text.match(/[a-zA-Z]+/g);
+
+  if(text.length === 0 || cleanText === null) {
+    return [];
+  }
+
+  let cleanTextArr = cleanText.join('').toLowerCase().split('');
+  let count = cleanTextArr.length;
+  let countedLetters = cleanTextArr.reduce((acc, currentValue) => {
+  
+    acc[currentValue] ? acc[currentValue]++ : acc[currentValue] = 1;
+    return acc;
+  }, {});
+  
+  return Object
+    .entries(countedLetters)
+    .sort((a, b) => a[0]
+      .localeCompare(b[0]))
+    .map(el => {
+  
+      return {
+        letter: el[0],
+        frequency: Number(((100*el[1]) /count).toFixed(1))
+      }
+    });
 };
 
 function LettersFrequency() {
